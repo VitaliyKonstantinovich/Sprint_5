6f6a8f9f105a1c82732db0ab8ae5e5fca2dcc32c
VitaliyKonstantinovich <VitaliyKonstantinovich@users.noreply.github.com>
2026-02-23 08:32:43 +0300

    Final fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,16 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
-    # устанавливаем драйвер для Chrome
-    service = Service(ChromeDriverManager().install())
-    
-    # открываем браузер
-    driver = webdriver.Chrome(service=service)
-    
+    # открываем браузер Chrome
+    driver = webdriver.Chrome()
+
     # разворачиваем окно на весь экран
     driver.maximize_window()
-    
+
     # передаем браузер в тест
     yield driver
-    
+
     # закрываем браузер после того, как тест отработает
     driver.quit()
